0c7a99ccf369af9a4617f42297c4636097c1d5b6
segger <johanna.lynn.lahti@gmail.com>
2022-01-07 18:09:02 +0100

    Validate form

--- www/register.js
+++ www/register.js
@@ -8,7 +8,7 @@ $(function() {
         return false;
     });
 
-    // $('#step2').hide();
+    $('#accordionStep2').hide();
     $('#step3').hide();
 
     $('#buttonStep1').click(function() {
@@ -18,7 +18,76 @@ $(function() {
         } else {
             $("#step1 :input").attr("disabled", true);
             $('#buttonStep1').hide();
-            $('#step2').show();
+            $('#accordionStep2').show();
+        }
+    });
+
+    function isValid(form) {
+        let valid = true;
+        let points = form.querySelectorAll("[id^='point']");
+        points.forEach(field => {
+            if (field.value && field.value > 25) {
+                valid = false;
+                field.setCustomValidity("Too high value of points");
+            } else {
+                field.setCustomValidity("");
+            }
+        });
+
+        let mm = form.querySelectorAll("[id^='time_mm']");
+        mm.forEach(field => {
+            if (field.value && field.value > 15) {
+                valid = false;
+                field.setCustomValidity("Too high value of mm");
+            } else {
+                field.setCustomValidity("");
+            }
+        });
+
+        let ss = form.querySelectorAll("[id^='time_ss']");
+        ss.forEach(field => {
+            if (field.value && field.value > 59) {
+                valid = false;
+                field.setCustomValidity("Too high value of ss");
+            } else {
+                field.setCustomValidity("");
+            }
+        });
+
+        let ms = form.querySelectorAll("[id^='time_ms']");
+        ms.forEach(field => {
+            if (field.value && field.value > 99) {
+                valid = false;
+                field.setCustomValidity("Too high value of ms");
+            } else {
+                field.setCustomValidity("");
+            }
+        });
+
+        return valid;
+    }
+
+    function allCollapsables(form, add) {
+        let collapsables = form.querySelectorAll(".collapse");
+        collapsables.forEach(c => {
+            if (add) {
+                c.classList.add('show');
+            } else {
+                c.classList.remove('show');
+            }
+        });
+    }
+
+    $('#buttonStep2').click(function() {
+        let form = document.querySelector('#step2');
+        if (!isValid(form)) {
+            form.classList.add('was-validated');
+            allCollapsables(form, true);
+        } else {
+            $("#step2 :input").attr("disabled", true);
+            allCollapsables(form, true);
+            $("#step3").show();
+            $('#buttonStep2').hide();
         }
     });
 });
