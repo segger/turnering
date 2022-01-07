520f770e4ee250cb32583fbdec11a41ceee12dae
segger <johanna.lynn.lahti@gmail.com>
2022-01-07 14:28:34 +0100

    Step1 of register

--- www/register.js
+++ www/register.js
@@ -7,5 +7,19 @@ $(function() {
         window.location.href = "/";
         return false;
     });
+
+    $('#step2').hide();
+    $('#step3').hide();
+
+    $('#buttonStep1').click(function() {
+        let form = document.querySelector('#step1');
+        if (!form.checkValidity()) {
+            form.classList.add('was-validated');
+        } else {
+            $("#step1 :input").attr("disabled", true);
+            $('#buttonStep1').hide();
+            $('#step2').show();
+        }
+    });
 });
 
